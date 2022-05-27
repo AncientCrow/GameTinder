ad594c2245cd4c5c24d201d00fbddabb6963f6d9
Crow <brainburst54@gmail.com>
2022-05-27 08:35:45 +0300

    Начальный коммит

---
+++ GameTinder/app_cards/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Card(models.Model):
+    icon = models.ImageField
+    name = models.CharField(max_length=25)
+    games = models.TextField(max_length=1500)
+    relation = models.ManyToOneRel()
