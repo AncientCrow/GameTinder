4665b1a6cbe592c292081739001eb01da6b8c411
Илья Горчаков <brainburst54@gmail.com>
2022-07-11 11:49:53 +0300

    Смена на pyTelegramBotAPI

--- GameTinder/app_cards/management/commands/bot.py
+++ GameTinder/app_cards/management/commands/bot.py
@@ -1,24 +1,14 @@
+import telebot
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from telegram import Bot
-from telegram import Update
-from telegram.ext import CallbackContext
-from telegram.ext import MessageHandler
-from telegram.ext import Updater
-from telegram.utils.request import Request
 
 
 class Command(BaseCommand):
     help = 'Telegram-bot'
 
     def handle(self, *args, **options):
-        request = Request(
-            connect_timeout=0.5,
-            read_timeout=1.0,
-        )
-        bot = Bot(
-            request=request,
-            token=settings.TOKEN,
-            base_url=settings.PROXY_URL
+
+        bot = telebot.TeleBot(
+            token=settings.TOKEN
         )
         print(bot.get_me())
--- GameTinder/app_cards/models.py
+++ GameTinder/app_cards/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    tg_id = models.PositiveIntegerField(
+        verbose_name='ID telegram пользователя',
+        unique=True,
+    )
+    name = models.CharField(
+        max_length=256,
+        verbose_name='имя пользователя',
+    )
+
+    def __str__(self):
+        return f'{self.tg_id} {self.name}'
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
 
 
 class Card(models.Model):
