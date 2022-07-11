459867abc3b30ac9b12f8b878f73b06b8dabf16b
Илья Горчаков <brainburst54@gmail.com>
2022-07-11 12:03:23 +0300

    добавление .env удаление ключей из settings

--- GameTinder/app_cards/management/commands/bot.py
+++ GameTinder/app_cards/management/commands/bot.py
@@ -1,14 +1,18 @@
 import telebot
-from django.core.management.base import BaseCommand
+
 from django.conf import settings
+from django.core.management.base import BaseCommand
+from dotenv import dotenv_values
 
 
 class Command(BaseCommand):
     help = 'Telegram-bot'
+    values = dotenv_values('.env')
 
     def handle(self, *args, **options):
 
         bot = telebot.TeleBot(
-            token=settings.TOKEN
+            token=self.values['TOKEN']
         )
+
         print(bot.get_me())
